041abb3c50b21c410288643a6f6c7c14ebec0df5
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-07-22 19:08:40 +0500

    routes

--- main.go
+++ main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"os"
 
+	"github.com/alphadev97/go-retaurant-management-backend/database"
+	"github.com/alphadev97/go-retaurant-management-backend/middleware"
+	"github.com/alphadev97/go-retaurant-management-backend/routes"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	routes.UserRoutes(router)
+	router.Use(middleware.Authentication())
+
+	routes.FoodRoutes(router)
+	routes.MenuRoutes(router)
+	routes.TableRoutes(router)
+	routes.OrderRoutes(router)
+	routes.OrderItemRoutes(router)
+	routes.InvoiceRoutes(router)
+
+	router.Run(":" + port)
 }
