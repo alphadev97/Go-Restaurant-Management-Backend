3a6a9ed7754b5297e8dfe6f2d75bae914331629f
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-07-23 22:18:53 +0500

    GetInvoices

--- controllers/invoiceController.go
+++ controllers/invoiceController.go
@@ -1,10 +1,45 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/alphadev97/go-restaurant-management-backend/database"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type InvoiceViewFormat struct {
+	Invoice_id       string
+	Payment_method   string
+	Order_id         string
+	Payment_status   *string
+	Payment_due      interface{}
+	Table_number     interface{}
+	Payment_due_date time.Time
+	Order_details    interface{}
+}
+
+var invoiceCollection *mongo.Collection = database.OpenCollection(database.Client, "invoice")
 
 func GetInvoices() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 
+		result, err := invoiceCollection.Find(context.TODO(), bson.M{})
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error while fetching the invoices"})
+		}
+
+		var allInvoices []bson.M
+		if err = result.All(ctx, &allInvoices); err != nil {
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOK, allInvoices)
 	}
 }
 
