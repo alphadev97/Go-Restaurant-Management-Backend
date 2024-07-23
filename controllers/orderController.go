0547bc6c5da24c8d2da047112dbf62d402edb318
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-07-23 21:23:38 +0500

    GetOrder

--- controllers/orderController.go
+++ controllers/orderController.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/alphadev97/go-restaurant-management-backend/database"
+	"github.com/alphadev97/go-restaurant-management-backend/models"
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -33,7 +34,16 @@ func GetOrders() gin.HandlerFunc {
 
 func GetOrder() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		orderId := c.Param("order_id")
+		var order models.Order
 
+		err := foodCollection.FindOne(ctx, bson.M{"order_id": orderId}).Decode(&order)
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while fetching the order"})
+		}
+		c.JSON(http.StatusOK, order)
 	}
 }
 
