77a27d18471da24e7c6f648f63df532b39c0b86d
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-07-24 04:18:18 +0500

    GetOrderItems

--- controllers/orderItemController.go
+++ controllers/orderItemController.go
@@ -1,13 +1,41 @@
 package controller
 
 import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/alphadev97/go-restaurant-management-backend/database"
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
+
+var orderItemCollection *mongo.Collection = database.OpenCollection(database.Client, "orderItem")
 
 func GetOrderItems() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 
+		result, err := orderItemCollection.Find(context.TODO(), bson.M{})
+
+		defer cancel()
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		var allOrderItem []bson.M
+
+		if err = result.All(ctx, &allOrderItem); err != nil {
+			log.Fatal(err)
+			return
+		}
+
+		c.JSON(http.StatusOK, allOrderItem)
 	}
 }
 
