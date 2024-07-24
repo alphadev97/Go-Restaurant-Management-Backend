a6f961c5db876b7b26660c5cf47b9b1e36c812ed
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-07-24 23:58:28 +0500

    UpdateTable

--- controllers/tableController.go
+++ controllers/tableController.go
@@ -13,6 +13,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var tableCollection *mongo.Collection = database.OpenCollection(database.Client, "table")
@@ -95,6 +96,46 @@ func CreateTable() gin.HandlerFunc {
 
 func UpdateTable() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
 
+		var table models.Table
+		tableId := c.Param("table_id")
+
+		if err := c.BindJSON(&table); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		var updatedObj primitive.D
+
+		if table.Number_of_guests != nil {
+			updatedObj = append(updatedObj, bson.E{Key: "number_of_guests", Value: table.Number_of_guests})
+		}
+
+		if table.Table_number != nil {
+			updatedObj = append(updatedObj, bson.E{Key: "table_number", Value: table.Table_number})
+		}
+
+		table.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+
+		upsert := true
+		opt := options.UpdateOptions{
+			Upsert: &upsert,
+		}
+
+		filter := bson.M{"table_id": tableId}
+
+		result, err := tableCollection.UpdateOne(ctx, filter, bson.D{{Key: "$set", Value: updatedObj}}, &opt)
+
+		if err != nil {
+			msg := fmt.Sprintf("table item update faild")
+			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+			return
+		}
+
+		defer cancel()
+
+		c.JSON(http.StatusOK, result)
 	}
 }
