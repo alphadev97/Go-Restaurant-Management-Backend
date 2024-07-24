c815b133d5e2761387e99afd88cc23a80ac6c043
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-07-24 23:02:27 +0500

    GetTables

--- controllers/tableController.go
+++ controllers/tableController.go
@@ -1,10 +1,36 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/alphadev97/go-restaurant-management-backend/database"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var tableCollection *mongo.Collection = database.OpenCollection(database.Client, "table")
 
 func GetTables() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 
+		result, err := tableCollection.Find(context.TODO(), bson.M{})
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Error occured while lsiting the table item"})
+		}
+
+		var allTables []bson.M
+
+		if err := result.All(ctx, &allTables); err != nil {
+			log.Fatal(err)
+		}
+
+		c.JSON(http.StatusOK, allTables)
 	}
 }
 
